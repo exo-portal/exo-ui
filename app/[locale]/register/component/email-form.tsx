5dff5809b0c1b8451109d4b6b09ed6887a21753f
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-25 22:04:31 +0800

    feat: adding auth form and email form with validation and ui design

--- app/[locale]/register/component/email-form.tsx
+++ app/[locale]/register/component/email-form.tsx
@@ -1,7 +1,107 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { translate } from "@/lib";
+import { useTranslations } from "next-intl";
+import FormFieldInput from "@/components/form-field-input";
+import Image from "next/image";
+import { UserIcon } from "@/components/icons";
 
 export default function EmailForm() {
+    const t = useTranslations();
+
+  const FormSchema = z
+    .object({
+      email: z
+        .string()
+        .email("Invalid email address")
+        .min(8, "Password must be at least 8 characters"),
+      password: z
+        .string()
+        .min(8, "Password must be at least 8 characters")
+        .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+        .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+        .regex(/[0-9]/, "Password must contain at least one number")
+        .regex(
+          /[^A-Za-z0-9]/,
+          "Password must contain at least one special character"
+        ),
+      confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    });
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    mode: "onChange",
+    reValidateMode: "onChange",
+  });
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log("Form submitted:", data);
+  };
+
   return (
-    <div>EmailForm</div>
-  )
+    <Form {...form}>
+      <form
+        className="flex flex-col gap-4"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        {/* EMAIL FIELD */}
+        <FormFieldInput
+          id={"email"}
+          name={"email"}
+          schema={FormSchema}
+          control={form.control}
+          inputSuffixIcon={<Image src={UserIcon} alt="user icon" />}
+          labelKey={"register.form.input.label.email"}
+          placeholderKey={"register.form.input.placeholder.email"}
+        />
+
+        {/* PASSWORD FIELD */}
+        <FormFieldInput
+          id={"email"}
+          name={"password"}
+          type={"password"}
+          schema={FormSchema}
+          control={form.control}
+          labelKey={"register.form.input.label.password"}
+          placeholderKey={"register.form.input.placeholder.password"}
+        />
+
+        {/* RE ENTER PASSWORD FIELD */}
+        <FormFieldInput
+          control={form.control}
+          schema={FormSchema}
+          name={"confirmPassword"}
+          id={"confirmPassword"}
+          placeholderKey={"register.form.input.placeholder.reEnterPassword"}
+          labelKey={"register.form.input.label.reEnterPassword"}
+          type="password"
+        />
+
+        {/* Submit */}
+        <Button type="submit">
+          {translate(t, "register.form.button.continue")}
+        </Button>
+      </form>
+
+      {/* Sign In Link */}
+      <div className="text-center">
+        Already have an account? <Button variant={"link"}>Sign In</Button>
+      </div>
+    </Form>
+  );
 }
