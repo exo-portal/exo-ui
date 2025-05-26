9dc23e70e74ff0b56f35193634fcda53d68b4001
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-26 09:18:54 +0800

    feat: adding react scan config for optimization

--- components/react-scan.tsx
+++ components/react-scan.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { DEV_MODE } from "@/config";
 import React, { JSX, useEffect } from "react";
 import { scan } from "react-scan";
 
 export default function ReactScan(): JSX.Element {
   useEffect(() => {
     scan({
-      enabled: true,
+      enabled: DEV_MODE == "development",
     });
   }, []);
 
