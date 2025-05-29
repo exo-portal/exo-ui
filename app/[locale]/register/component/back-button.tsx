02214a2afc3771d321480d27becc70078ededa2a
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-29 18:32:49 +0800

    chore: adding type button to back button to prevent unexpected submision behavior if the component is used within form

--- app/[locale]/register/component/back-button.tsx
+++ app/[locale]/register/component/back-button.tsx
@@ -19,6 +19,7 @@ export default function BackButton() {
 
   return (
     <button
+      type="button"
       className="flex gap-2 px-3.5 py-2.5 cursor-pointer items-center"
       onClick={onClickHandler}
     >
