6646f1acab459fdcadd86e1125534425c0000592
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-30 14:59:20 +0800

    feat: adding client only provider to login email

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { DividerForm, AuthForm, EmailForm } from "./components";
+import { ClientOnlyProvider } from "@/providers";
 
 export default function page() {
   return (
     <div className="flex flex-col gap-9" data-testid="login-page">
       <AuthForm data-testid="auth-form" />
       <DividerForm data-testid="divider-form" />
-      <EmailForm data-testid="email-form" />
+      <ClientOnlyProvider fallback={<div>loading...</div>}>
+        <EmailForm data-testid="email-form" />
+      </ClientOnlyProvider>
     </div>
   );
 }
