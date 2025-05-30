3a9b5a1e90a9ea04c6200fb2eb808dc9d16c1410
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-30 15:05:09 +0800

    feat: adding skeleton fallback for login email

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { DividerForm, AuthForm, EmailForm } from "./components";
 import { ClientOnlyProvider } from "@/providers";
+import { EmailFormSkeleton } from "./components";
 
 export default function page() {
   return (
     <div className="flex flex-col gap-9" data-testid="login-page">
       <AuthForm data-testid="auth-form" />
       <DividerForm data-testid="divider-form" />
-      <ClientOnlyProvider fallback={<div>loading...</div>}>
+      <ClientOnlyProvider fallback={<EmailFormSkeleton />}>
         <EmailForm data-testid="email-form" />
       </ClientOnlyProvider>
     </div>
