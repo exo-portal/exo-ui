d010b226597a2b23d6c6c1f1ff837fc3c6ab55eb
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-30 14:03:45 +0800

    test: add test jest for login layout and page

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -5,10 +5,10 @@ import { DividerForm, AuthForm, EmailForm } from "./components";
 export default function page() {
   const t = useTranslations();
   return (
-    <div className="flex flex-col gap-9">
-      <AuthForm />
-      <DividerForm />
-      <EmailForm />
+    <div className="flex flex-col gap-9" data-testid="login-page">
+      <AuthForm data-testid="auth-form" />
+      <DividerForm data-testid="divider-form" />
+      <EmailForm data-testid="email-form" />
     </div>
   );
 }
