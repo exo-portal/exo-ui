c908a72335df3ed8248578d164229aba434fe7b5
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-30 14:44:28 +0800

    chore: remove unused variable and import for translations in login page

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { useTranslations } from "next-intl";
 import { DividerForm, AuthForm, EmailForm } from "./components";
 
 export default function page() {
-  const t = useTranslations();
   return (
     <div className="flex flex-col gap-9" data-testid="login-page">
       <AuthForm data-testid="auth-form" />
