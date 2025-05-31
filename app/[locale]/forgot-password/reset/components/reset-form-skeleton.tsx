a7668b1c8f64722553e6aff62ea3c9b0bc36f1ac
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-31 21:20:25 +0800

    feat: add reset form skeleton fallback

---
+++ app/[locale]/forgot-password/reset/components/reset-form-skeleton.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+
+export function ResetFormSkeleton() {
+  return (
+    <div className="flex flex-col gap-4">
+      <span className="sr-only">Loading Reset Password form</span>
+      {/* Password Field */}
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-4 w-38" />
+        <Skeleton className="h-10 w-full rounded-xl" />
+      </div>
+      {/* Password Field */}
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-4 w-44" />
+        <Skeleton className="h-10 w-full rounded-xl" />
+      </div>
+      {/* Button */}
+      <Skeleton className="h-10 w-full rounded-xl" />
+    </div>
+  );
+}
