ea46044e17682537435c0c5ee90aaa880ec3f7ad
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-31 21:00:37 +0800

    feat: adding skeleton for otp form

---
+++ app/[locale]/forgot-password/otp/components/otp-form-skeleton.tsx
@@ -0,0 +1,25 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+
+export function OtpFormSkeleton() {
+  return (
+    <div
+      className="flex flex-col gap-16"
+      role="status"
+      aria-busy="true"
+      data-testid="otp-form-skeleton"
+    >
+      <div className="flex w-full justify-center">
+        <div className="flex gap-4 w-9/12">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <Skeleton key={index} className="h-22 w-full rounded-xl" />
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 w-full items-center">
+        <Skeleton className="h-10 w-full rounded-xl" />
+        <Skeleton className="h-4 w-7/12 2 rounded-xl" />
+      </div>
+    </div>
+  );
+}
