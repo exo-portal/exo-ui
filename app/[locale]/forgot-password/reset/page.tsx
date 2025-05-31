bca2671dfaaf9b4bd709767d4a7e92a64469cc80
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-31 21:20:25 +0800

    feat: add reset form skeleton fallback

--- app/[locale]/forgot-password/reset/page.tsx
+++ app/[locale]/forgot-password/reset/page.tsx
@@ -1,14 +1,14 @@
 import { BackButton } from "@/components/common";
 import { ClientOnlyProvider } from "@/providers";
 import React from "react";
-import { ResetForm, ResetHeader } from "./components";
+import { ResetForm, ResetFormSkeleton, ResetHeader } from "./components";
 
 export default function ResetPage() {
   return (
     <section className="flex flex-col flex-1 justify-center">
       <BackButton />
       <ResetHeader />
-      <ClientOnlyProvider fallback={<div>loading...</div>}>
+      <ClientOnlyProvider fallback={<ResetFormSkeleton />}>
         <ResetForm />
       </ClientOnlyProvider>
     </section>
