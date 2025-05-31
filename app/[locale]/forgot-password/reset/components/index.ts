1d317d57c629473550d862a6aa758679a63306ee
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-31 19:28:23 +0800

    feat: add ui for reset password page

---
+++ app/[locale]/forgot-password/reset/components/index.ts
@@ -0,0 +1,2 @@
+export * from "./reset-form";
+export * from "./reset-header";
--- app/[locale]/forgot-password/reset/page.tsx
+++ app/[locale]/forgot-password/reset/page.tsx
@@ -1,5 +1,16 @@
+import { BackButton } from "@/components/common";
+import { ClientOnlyProvider } from "@/providers";
 import React from "react";
+import { ResetForm, ResetHeader } from "./components";
 
 export default function ResetPage() {
-  return <div>ResetPage</div>;
+  return (
+    <section className="flex flex-col flex-1 justify-center">
+      <BackButton />
+      <ResetHeader />
+      <ClientOnlyProvider fallback={<div>loading...</div>}>
+        <ResetForm />
+      </ClientOnlyProvider>
+    </section>
+  );
 }
