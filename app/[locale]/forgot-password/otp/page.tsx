70fb6bb5d02095d29e21865c87c41ea0b30783fa
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-31 21:00:37 +0800

    feat: adding skeleton for otp form

--- app/[locale]/forgot-password/otp/page.tsx
+++ app/[locale]/forgot-password/otp/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { ClientOnlyProvider } from "@/providers";
 import { BackButton } from "@/components/common";
-import { OtpForm, OtpHeader } from "./components";
+import { OtpForm, OtpFormSkeleton, OtpHeader } from "./components";
 
 export default function OtpPage() {
   return (
     <section className="flex flex-col flex-1">
       <BackButton />
       <OtpHeader />
-      <ClientOnlyProvider fallback={<div>loading...</div>}>
+      <ClientOnlyProvider fallback={<OtpFormSkeleton />}>
         <OtpForm />
       </ClientOnlyProvider>
     </section>
