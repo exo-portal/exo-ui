409dad8ea0d9d21b8c8d7401a69d948bbb5c1878
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-31 17:02:12 +0800

    refactor: use DEFAULT_OTP_LENGTH from config in OTP validation schema for consistency

--- app/[locale]/forgot-password/otp/validation/otp-form-validation-schema.tsx
+++ app/[locale]/forgot-password/otp/validation/otp-form-validation-schema.tsx
@@ -1,7 +1,8 @@
+import { DEFAULT_OTP_LENGTH } from "@/config";
 import { z } from "zod";
 
 export const OtpFormSchema = z.object({
-  pin: z.string().length(4, {
+  pin: z.string().length(DEFAULT_OTP_LENGTH, {
     message: "Your one-time password must be 4 characters.",
   }),
 });
