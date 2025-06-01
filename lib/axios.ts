ae2db64fc0f74c939f33168698e1c13fc925ca96
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-01 23:43:51 +0800

    feat: Workflow for Exo Login Backend
    - Frontend can now call backend login api
    - Can catch if user is invalid using the Custom Exo Exception
    - Will display error message from backend i the input message validation error

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,8 @@
+import { BASE_URL } from "@/config";
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: BASE_URL,
   withCredentials: true, // Important: Send cookies with requests
   timeout: 5000, // Set timeout to 5000ms (5 seconds)
 });
